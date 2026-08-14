01b9b97eed0514a15dd66daf907bda5e55dcae07
KunwarSidhu47 <kunwarsidhu1947@gmail.com>
2026-08-15 01:44:41 +0530

    Fix wildcard route for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -38,7 +38,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  // Use regex for wildcard to support Express 5 / latest path-to-regexp
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
   });
 }
